4fe87f06ce20341e2aabeb21f65aa8476dac65dd
Direnc Timur <direnc99@gmail.com>
2020-05-31 01:43:12 +0200

    #11: enhanced random danbooru post

--- src/main/java/imageboards/api/DanbooruHandler.java
+++ src/main/java/imageboards/api/DanbooruHandler.java
@@ -1,10 +1,13 @@
 package imageboards.api;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 import imageboards.exceptions.ImageNotFoundException;
+import kong.unirest.GetRequest;
 import kong.unirest.json.JSONArray;
 import kong.unirest.json.JSONObject;
 import shared.api.BasicApi;
@@ -14,44 +17,75 @@ public class DanbooruHandler extends BasicApi {
     final String QUERY_LIMIT = "limit";
     final String QUERY_ENCODING = "utf8";
     final String QUERY_TAGS = "tags";
+    final String QUERY_RANDOM = "random";
     final String POST_FILE_URL = "file_url";
     final String QUERY_TAGS_SEARCH_HIDE_EMPTY = "search[hide_empty]";
     final String QUERY_TAGS_SEARCH_ORDER = "search[order]";
     final String QUERY_TAGS_SEARCH_NAME_MATCHES = "search[name_matches]";
     final String TAG_NAME = "name";
 
-    public String getImageByQuery(String tag) throws ImageNotFoundException {
-        JSONArray response = this.baseGetRequest(DanbooruUrlFactory.getPostsUrl())
-                .queryString(QUERY_ENCODING, "%E2%9C%93").queryString(QUERY_LIMIT, "200").queryString(QUERY_TAGS, tag)
+    private GetRequest basicImageByQueryRequest(String tag) {
+        return this.baseGetRequest(DanbooruUrlFactory.getPostsUrl()).queryString(QUERY_ENCODING, "%E2%9C%93")
+                .queryString(QUERY_TAGS, tag);
+    }
+
+    private GetRequest basicImageByQueryRequest(String tag1, String tag2) {
+        return this.baseGetRequest(DanbooruUrlFactory.getPostsUrl()).queryString(QUERY_ENCODING, "%E2%9C%93")
+                .queryString(QUERY_TAGS, tag1 + " " + tag2);
+    }
+
+    public String getRandomImageByQuery(String tag) throws ImageNotFoundException {
+        JSONArray response = basicImageByQueryRequest(tag).queryString(QUERY_LIMIT, 1).queryString(QUERY_RANDOM, true)
                 .asJson().getBody().getArray();
         if (response.length() < 1) {
             throw new ImageNotFoundException();
         }
-        JSONObject randomPostOfReponse = getRandomPost(response);
-        String imageUrl = getImageUrlOfPost(randomPostOfReponse);
+        JSONObject randomPost = response.getJSONObject(0);
+        String imageUrl = getImageUrlOfPost(randomPost);
         return imageUrl;
     }
 
-    public String getImageByQuery(String tag1, String tag2) throws ImageNotFoundException {
-        JSONArray response = this.baseGetRequest(DanbooruUrlFactory.getPostsUrl())
-                .queryString(QUERY_ENCODING, "%E2%9C%93").queryString(QUERY_LIMIT, "200")
-                .queryString(QUERY_TAGS, new String[] { tag1, tag2 }).asJson().getBody().getArray();
+    public String getRandomImageByQuery(String tag1, String tag2) throws ImageNotFoundException {
+        JSONArray response = basicImageByQueryRequest(tag1, tag2).queryString(QUERY_LIMIT, "1")
+                .queryString(QUERY_RANDOM, true).asJson().getBody().getArray();
         if (response.length() < 1) {
             throw new ImageNotFoundException();
         }
-        JSONObject randomPostOfReponse = getRandomPost(response);
-        String imageUrl = getImageUrlOfPost(randomPostOfReponse);
+        JSONObject randomPost = response.getJSONObject(0);
+        String imageUrl = getImageUrlOfPost(randomPost);
         return imageUrl;
+    }
+
+    public List<String> getImageUrlsByQuery(String tag) throws ImageNotFoundException {
+        JSONArray response = basicImageByQueryRequest(tag).queryString(QUERY_LIMIT, "200").asJson().getBody()
+                .getArray();
+        if (response.length() < 1) {
+            throw new ImageNotFoundException();
+        }
+        List<String> imageUrls = new ArrayList<>();
+        response.forEach(postObject -> {
+            JSONObject post = (JSONObject) postObject;
+            imageUrls.add(getImageUrlOfPost(post));
+        });
+        return imageUrls;
+    }
+
+    public List<String> getImageUrlsByQuery(String tag1, String tag2) throws ImageNotFoundException {
+        JSONArray response = basicImageByQueryRequest(tag1, tag2).queryString(QUERY_LIMIT, "200").asJson().getBody()
+                .getArray();
+        if (response.length() < 1) {
+            throw new ImageNotFoundException();
+        }
+        List<String> imageUrls = new ArrayList<>();
+        response.forEach(postObject -> {
+            JSONObject post = (JSONObject) postObject;
+            imageUrls.add(getImageUrlOfPost(post));
+        });
+        return imageUrls;
     }
 
     private String getImageUrlOfPost(JSONObject post) {
         return post.getString(POST_FILE_URL);
-    }
-
-    private JSONObject getRandomPost(JSONArray posts) {
-        Random random = new Random();
-        int rand = random.nextInt(posts.length());
-        return (JSONObject) posts.get(rand);
     }
 
     public JSONArray getTagsByQuery(String query) {
