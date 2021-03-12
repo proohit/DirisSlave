3b93bab81fb941fae104fdc2468b47ef98b6385b
proohit <direnc99@gmail.com>
2021-03-12 20:50:59 +0100

    Catch NaN inputs for dice

--- src/main/java/dice/commands/DiceRollCommand.java
+++ src/main/java/dice/commands/DiceRollCommand.java
@@ -14,13 +14,17 @@ public class DiceRollCommand extends Command {
         addCommendPrefix("roll");
         setTopic("util");
         setDescription("Roll a dice");
-        setHelpString("<Maximum number to roll. E.g. for a D6 dice, input 6>");
+        setHelpString("<Maximum number to roll. E.g. for a D6 dice, input 6 or d6>");
         setMinArguments(1);
     }
 
     @Override
     protected void handleImpl(MessageReceivedEvent event, String[] argStrings) {
-        int maximumNumber = Integer.parseInt(argStrings[0].replace("d", ""));
-        CommandManager.sendBeautifulMessage(event, String.format("%s", random.nextInt(maximumNumber) + 1));
+        try {
+            int maximumNumber = Integer.parseInt(argStrings[0].replace("d", ""));
+            CommandManager.sendBeautifulMessage(event, String.format("%s", random.nextInt(maximumNumber) + 1));
+        } catch (NumberFormatException e) {
+            CommandManager.sendMessage(event, getHelp());
+        }
     }
 }
